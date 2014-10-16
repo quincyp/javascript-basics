d51b24d9105c9d0a4a03d9829c7b3bfd8dcad3da
quincyp <quincytpham@gmail.com>
2014-10-16 10:02:39 -0700

    Added inspect element -> console viewable items, inserted dog image before body of text

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@
 	Javascript 
 	Comment
 */
+"use strict";
+var person = {
+	fName: 'Quincy',
+	lName: 'Pham',
+	email: 'quincytpham@gmail.com',
+	getFullName: function() { return this.fName + ' ' + this.lName;}
+};
+
+//NEVER USE ALERT
+//alert(person.getFullNames());
+console.log('running the getFullName function on person object');
+console.log(person.getFullName());
+
+console.log('looping through all properties of therson object');
+//show all properties of the person object
+for (var prop in person) {
+	console.log(person[prop]); // => value of current property
+}
+
+//create a new img element
+var elem = document.createElement('img');
+elem.src = 'img/dog.jpg';
+elem.alt = 'picture of my awesome, amazing dog';
+
+//add this element to the top of the body
+document.body.insertBefore(elem, document.body.firstChild);
